d3184d1591c81cfea3332347f7a7b47fe8822eb8
Aderianna Williams <aderiannaw@yahoo.com>
2019-03-03 23:47:09 +0000

    Automatically backed up by Learn

--- lib/musicLibraryController.rb
+++ lib/musicLibraryController.rb
@@ -80,7 +80,7 @@ end
   puts "Which song number would you like to play?"
   input = gets.strip.to_i
   songs = Song.all(input)
-  puts "Playing #{song.name} by #{song.artist}" if song
+  puts "Playing #{song.name} by #{song.artist}" 
   
 
   end
