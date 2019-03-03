72c1da8394d2a4463e004c0d2cf1fcf7bcaa4edc
Aderianna Williams <aderiannaw@yahoo.com>
2019-03-03 21:48:50 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,42 +1,43 @@
-class Genre 
-  extend Concerns::Findable
-
-  attr_accessor  :name, :songs
-  
-   @@all = []
+class Genre
+   extend Concerns::Findable 
    
-def initialize(name)
-  @name = name
-  @songs = []
-  save
+  attr_accessor :name
+  attr_reader :songs 
   
-end
-
-def self.all
-  @@all
-end
-
-def self.destroy_all
-  all.clear
+  @@all = []  
   
-end
+  
+  def initialize(name)
+    @name = name 
+    @songs= [] 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.destroy_all
+    @@all.clear 
+  end 
 
-def save
-  @@all << self
-end
-
-def self.create(name)
-  genre = new(name)
-  genre.save
-  genre
-end
-
+  def save
+  @@all << self 
+  self  
+  end 
+  
+  def self.create(name)  
+  song = new(name)
+  song.save 
+  song
+ end 
+ 
+ def songs
+   @songs
+ end 
+ 
  def artists
-   artists =  self.songs.collect do |song|
-      song.artist
-    end
-    artists.uniq
-  end
-
-
+  songs.map(&:artist).uniq   
+ end 
+ 
+  
 end
