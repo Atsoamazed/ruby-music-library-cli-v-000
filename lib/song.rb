10bff90ccf18cb1c5a59ba57ddecc1490ba8614a
Aderianna Williams <aderiannaw@yahoo.com>
2019-03-03 23:45:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,77 +1,84 @@
-class Song
-
-  attr_accessor  :name, :artist, :genre
-   @@all = []
- extend Concerns::Findable
-
-def initialize(name, artist = nil, genre = nil)
-  @name = name
-   self.artist = artist if artist
-   self.genre = genre if genre
-
-end
-
-
-
-def artist=(artist)
-  @artist = artist
-  artist.add_song(self)
-end
-
-def genre=(genre)
-  @genre = genre
-  genre.songs << self unless genre.songs.include?(self)
-end
-
-def self.all
-  @@all
-end
-
-def self.destroy_all
-  all.clear
-
-end
-
-def save
-  @@all << self
-end
-
-
- def self.find_by_name(name)
-    self.all.detect {|song| song.name.eql?(name)}
-  end
+require 'pry'
+class Song 
+   
+   
+  attr_accessor :name
+  attr_reader :artist, :genre  
   
-
-  def self.find_or_create_by_name(name)
-   find_by_name(name) || create(name)
+  @@all =[]             #is initialized as an empty array
+  
+  def initialize(name, artist = nil, genre = nil )
+    @name= name 
+    self.artist = artist if artist 
+    self.genre = genre if genre
   end 
   
-def self.create(name)
-  song = new(name)
-  song.save
-  song
-end
-
-def self.new_from_filename(filename)
-  artist, song, genre = filename.sub('.mp3', '').split(' - ')
-  artist = Artist.find_or_create_by_name(artist)
-  genre = Genre.find_or_create_by_name(genre)
-  new(song, artist, genre)
-end
-
-def self.create_from_filename(filename)
-     new_song = self.new_from_filename(filename).save
-    new_song
-
-end
-
-def add_artist(artist)
-  if artist.song != self
-  artist.song = self
-end
-if !@artists.include?(artist)
-  @artists << artist
-end
-end
+  def self.all          #returns the class variable @@all
+    @@all 
+  end 
   
+  
+  def self.destroy_all  #resets the @@all class variable to an empty array
+   @@all.clear
+  end 
+  
+  def  save            #adds the Song instance to the @@all class variable
+   @@all << self  
+   self 
+  end 
+  
+
+ def self.create(name)  #initializes, saves, and returns the song
+  song = new(name)
+  song.save 
+  song
+ end 
+ 
+ def artist
+   @artist
+ end 
+ 
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end 
+  
+  def genre=(genre)
+    @genre = genre 
+    genre.songs.push self unless genre.songs.include? self 
+  end 
+  
+  def self.find_by_name(name) #finds a song instance in @@all by the name property of the song
+    #@@all.detect do |song|
+      #song.name == name 
+      all.find { |song| song.name == name }
+  
+ end 
+ 
+ def self.find_or_create_by_name(name)
+   self.find_by_name(name) || create(name) #invokes .create instead of re-coding the same functionality
+   
+ end 
+ 
+ def self.create_by_name(name)
+   self.new(name).save
+ end
+ 
+ def self.new_from_filename(name)
+  artist, song, genre_name = name.split(' - ')
+  fixed_name = genre_name.gsub('.mp3', '')
+
+   artist = Artist.find_or_create_by_name(artist)
+   genre = Genre.find_or_create_by_name(fixed_name)
+   new(song, artist, genre)
+ end
+ 
+  def self.create_from_filename(name)
+   new_from_filename(name).save
+   
+  end 
+ 
+ 
+
+ 
 end
