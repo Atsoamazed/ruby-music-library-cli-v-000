e6898cdb67c7c8b9e613de095d7297b910b1b947
Aderianna Williams <aderiannaw@yahoo.com>
2019-03-03 21:42:29 +0000

    Automatically backed up by Learn

--- lib/musicLibraryController.rb
+++ lib/musicLibraryController.rb
@@ -47,7 +47,7 @@ class MusicLibraryController
     end
     
     def list_genres
-      Genre.all.sort_by.each.with_index(1) do |genre, i|
+      Genre.all.sort_by(&:name).each.with_index(1) do |genre, i|
       puts "#{i}. #{genre.name}"
     end
     end
